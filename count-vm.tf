35b31115acb7f9652b210839bc849346957bdff1
SavkinILYA <47076887+SavkinILYA@users.noreply.github.com>
2026-03-20 15:22:55 +0300

    Parameterize VM configuration in count-vm.tf

--- count-vm.tf
+++ count-vm.tf
@@ -1,24 +1,24 @@
 data "yandex_compute_image" "ubuntu_count" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
 
 resource "yandex_compute_instance" "web" {
   count       = 2
   name        = "web-${count.index + 1}"
-  platform_id = "standard-v2"
+  platform_id = var.vm_web_platform_id
   zone        = var.default_zone
 
   depends_on = [yandex_compute_instance.db]
 
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu_count.image_id
-      size     = 8
+      size     = var.vm_web_disk_size
     }
   }
   scheduling_policy {
