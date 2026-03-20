33295d0c03e7507e2118cef0c434d24033c789e3
SavkinILYA <47076887+SavkinILYA@users.noreply.github.com>
2026-03-20 15:21:58 +0300

    Parameterize disk and instance configurations

--- disk_vm.tf
+++ disk_vm.tf
@@ -1,28 +1,28 @@
 resource "yandex_compute_disk" "storage" {
-  count = 3
+  count = var.vm_storage_disk_count
   name  = "disk-${count.index + 1}"
-  size  = 1
+  size  = var.vm_storage_disk_size
   zone  = var.default_zone
 }
 
 data "yandex_compute_image" "ubuntu_storage" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
 
 resource "yandex_compute_instance" "storage" {
   name        = "storage"
-  platform_id = "standard-v2"
+  platform_id = var.vm_web_platform_id
   zone        = var.default_zone
 
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_storage_cores
+    memory        = var.vm_storage_memory
+    core_fraction = var.vm_storage_core_fraction
   }
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu_storage.image_id
-      size     = 8
+      size     = var.vm_storage_boot_disk_size
     }
   }
   dynamic "secondary_disk" {
